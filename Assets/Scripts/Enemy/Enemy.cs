e5c36b983abe426273dd852f28943e4bd40779cd
Karlof #1 <zurekdawid2@gmail.com>
2023-10-06 14:04:56 +0200

    soldier patroling system

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,6 +7,11 @@ public class Enemy : MonoBehaviour
 {
     private NavMeshAgent agent;
     private WeaponController player;
+    [SerializeField] private float distanceToFollow;
+
+    [SerializeField] private EnemyBehaviour enemyBehaviour;
+    private Transform currentPatrolPoint;
+    [SerializeField] private List<Transform> patrolPoints;
 
     private void Start()
     {
@@ -16,6 +21,40 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        agent.SetDestination(player.transform.position);
+        if(Vector3.Distance(player.transform.position, transform.position) < distanceToFollow)
+        {
+            enemyBehaviour = EnemyBehaviour.Following;
+            agent.SetDestination(player.transform.position);
+            currentPatrolPoint = null;
+        }
+        else
+        {
+            enemyBehaviour = EnemyBehaviour.Patroling;
+
+            if(currentPatrolPoint == null)
+            {
+                currentPatrolPoint = GetPatrolPoint();
+            }
+            else
+            {
+                agent.SetDestination(currentPatrolPoint.position);
+
+                if(Vector3.Distance(currentPatrolPoint.position, transform.position) < 2)
+                {
+                    currentPatrolPoint = GetPatrolPoint();
+                }
+            }
+        }
     }
+
+    private Transform GetPatrolPoint()
+    {
+        return patrolPoints[Random.Range(0, patrolPoints.Count)];
+    }
+}
+
+public enum EnemyBehaviour
+{
+    Following,
+    Patroling
 }
